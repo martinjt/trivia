3f5064551d99884b06060f1b4c1697fa8df425c1
Martin Thwaites <maritn.thwaites@interact-intranet.com>
2016-07-23 11:36:28 +0100

    initial commit

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -7,43 +7,44 @@ using UglyTrivia;
 
 namespace Trivia
 {
-    public class GameRunner
-    {
+	public class GameRunner
+	{
 
-        private static bool notAWinner;
+		private static bool notAWinner;
 
-        public static void Main(String[] args)
-        {
-            Game aGame = new Game();
+		public static void Main(String[] args)
+		{
+			Game aGame = new Game();
 
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
+			aGame.add("Chet");
+			aGame.add("Pat");
+			aGame.add("Sue");
 
-            Random rand = new Random();
+			var rand = new Random();
+			if (args.Length > 0)
+				rand = new Random(int.Parse(args[0]));
 
-            do
-            {
+			do
+			{
 
-                aGame.roll(rand.Next(5) + 1);
+				aGame.roll(rand.Next(5) + 1);
 
-                if (rand.Next(9) == 7)
-                {
-                    notAWinner = aGame.wrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.wasCorrectlyAnswered();
-                }
+				if (rand.Next(9) == 7)
+				{
+					notAWinner = aGame.wrongAnswer();
+				}
+				else
+				{
+					notAWinner = aGame.wasCorrectlyAnswered();
+				}
+			} while (notAWinner);
+
+			if (args.Length == 0)
+				Console.ReadLine();
+		}
 
 
-
-            } while (notAWinner);
-
-        }
-
-
-    }
+	}
 
 }
 
---
+++ C#/Trivia/Trivia/GameTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using UglyTrivia;
+
+namespace Trivia
+{
+	[TestFixture]
+	public class GameTest
+	{
+		[Test]
+		public void Game_not_playable_for_fewer_than_two_players()
+		{
+			var game = new Game();
+			Assert.That(game.isPlayable(), Is.False);
+			game.add("Keith");
+			Assert.That(game.isPlayable(), Is.False);
+			game.add("martin");
+			Assert.That(game.isPlayable(), Is.True);
+			game.add("Mark");
+			Assert.That(game.isPlayable(), Is.True);
+		}
+
+		[Test]
+		public void Game_can_be_won()
+		{
+			var game = new Game();
+			game.add("Keith");
+			game.add("martin");
+			game.add("Mark");
+
+			var hasbeenwon = false;
+
+			game.roll(5);
+			hasbeenwon = game.wasCorrectlyAnswered();
+
+			Assert.That(hasbeenwon, Is.True);
+		}
+
+		[Test]
+		public void GoldenMasterTest()
+		{
+			var filename = $"golden-{DateTime.Now.ToFileTime()}.txt";
+
+			using (var ms = new MemoryStream())
+			using (var writer = new StreamWriter(ms))
+			using (var fileStream = File.Create(filename))
+			{
+				Console.SetOut(writer);
+				for (var i = 0; i < 1000; i++)
+				{
+					GameRunner.Main(new[] { i.ToString() });
+				}
+				ms.WriteTo(fileStream);
+				ms.Flush();
+			}
+
+			var golden = File.ReadAllLines("goldenmaster.txt");
+			var runFile = File.ReadAllLines(filename);
+
+			for (var i = 0; i < golden.Length - 1; i++)
+			{
+				Assert.That(golden[i], Is.EqualTo(runFile[i]));
+			}
+		}
+	}
+}
